cba2ee57c524c0ed9b8b89442bd03317405934ea
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-09 21:00:15 +0530

    usr bookings

--- Header.php
+++ Header.php
@@ -1,40 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href = "style/Header.css">
+    <link rel="stylesheet" href="style/Header.css">
 </head>
+
 <body>
-<body>
-    <header>
-        <div class="logo-sign">
-            <div class="logo"></div>
-            <div class="sign">MotorBeam</div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="#fleet">Fleet</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <?php
-                if (isset($_SESSION['username'])) {
-                    $username = $_SESSION['username'];
-                    echo
-                    "
-                    <li>$username</li>
+
+    <body>
+        <header>
+            <div class="logo-sign">
+                <div class="logo"></div>
+                <div class="sign">MotorBeam</div>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#fleet">Fleet</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <?php
+                    if (isset($_SESSION['username'])) {
+                        $username = $_SESSION['username'];
+                        echo
+                        "
+                    <li><a href = 'userbooking.php'>{$username}'s bookings</a></li>
                      <li><a href='logout.php'>Logout</a></li>
                     ";
-                } else {
-                    echo "
+                    } else {
+                        echo "
                     <li><a href='Signup.php'>Signup</a></li> 
                     <li><a href='login.php'>Login</a></li>
                     ";
-                    
-                }
-                ?>
-            </ul>
-        </nav>
-    </header>
-</body>
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </header>
+    </body>
+
 </html>
